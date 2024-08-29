247c5c0cc51dadc1e109d0c4430df14dbae074f9
Kateryna <koliesnikova.kateryna@gmail.com>
2024-08-29 13:43:51 +0300

    start

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
