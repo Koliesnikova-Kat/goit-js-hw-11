322e92ded844899911524c2b6d5c4eec1f00b652
Kateryna <koliesnikova.kateryna@gmail.com>
2024-08-30 13:07:26 +0300

    finished

--- src/main.js
+++ src/main.js
@@ -1,7 +1,62 @@
+import {fetchedData} from './js/pixabay-api';
+import {createGalleryMarkup} from './js/render-functions';
 import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-
 import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const loader = document.querySelector('.loader');
+const gallery = document.querySelector('.gallery');
 
+const onFormSubmit = event => {
+  event.preventDefault();
+
+  const searchedValue = input.value.trim();
+
+  if (!searchedValue) {
+    iziToast.error({
+      message: 'Please enter a search query!',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  loader.classList.remove('is-hidden');
+
+  fetchedData(searchedValue)
+    .then(data => {
+      gallery.innerHTML = '';
+
+      if (data.hits.length === 0) {
+        iziToast.error({
+          message: 'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+
+        form.reset();
+        return;
+      }
+
+      const galleryMarkup = data.hits.map(createGalleryMarkup).join('');
+      gallery.innerHTML = galleryMarkup;
+
+      let galleryLibrary = new SimpleLightbox('.gallery li', {
+        captions: true,
+        captionsData: 'alt',
+        captionDelay: 250,
+        overlayOpacity: 0.8,
+        captionsPosition: 'bottom'
+      });
+
+      galleryLibrary.refresh();
+    })
+    .catch(err => {
+      console.log(err);
+    })
+    .finally(() => {
+      loader.classList.add('is-hidden');
+      form.reset();
+    });;
+}
+
+form.addEventListener('submit', onFormSubmit);
